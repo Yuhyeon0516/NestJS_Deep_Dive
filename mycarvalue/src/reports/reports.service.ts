fdac7d55086bbda5fae847736a99f9b8c8248290
Yuhyeon0516 <rladbgus0516@naver.com>
2024-01-11 17:46:19 +0900

    Created create report function

--- mycarvalue/src/reports/reports.service.ts
+++ mycarvalue/src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Report } from "./report.entity";
+import { Repository } from "typeorm";
+import { CreateReportDto } from "./dtos/create-report.dto";
+import { User } from "src/users/user.entity";
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+    constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+    create(reportDtoBody: CreateReportDto, user: User) {
+        const report = this.repo.create(reportDtoBody);
+        report.user = user;
+
+        return this.repo.save(report);
+    }
+}
