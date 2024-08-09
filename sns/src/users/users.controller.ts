2fb8c3c9f62a19feb68d1b8d0d71eb25c9c40941
Yuhyeon0516 <rladbgus0516@naver.com>
2024-08-09 21:42:36 +0900

    Created auth resource.

--- sns/src/users/users.controller.ts
+++ sns/src/users/users.controller.ts
@@ -1,18 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userseService: UsersService) {}
-
-  @Post()
-  postUser(
-    @Body('nickname') nickname: string,
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    return this.userseService.createUser(nickname, email, password);
-  }
 
   @Get()
   getUsers() {
