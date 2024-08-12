933e69727c14d71959621026d7c526f4f5e9de54
Yuhyeon0516 <rladbgus0516@naver.com>
2024-08-13 08:06:40 +0900

    created paginate-post dto

---
+++ sns/src/posts/dto/paginate-post.dto.ts
@@ -0,0 +1,20 @@
+import { IsIn, IsNumber, IsOptional } from 'class-validator';
+
+export class PaginatePostDto {
+  // 이전 마지막 데이터의 id
+  // 이 id보다 높은 id를 가져오기
+  @IsNumber()
+  @IsOptional()
+  where__id_more_than?: number;
+
+  // 정렬
+  // createdAt -> 생성된 시간의 내림차/오름차 순으로 정렬
+  @IsIn(['ASC', 'DESC'])
+  @IsOptional()
+  order__createdAt = 'ASC';
+
+  // 몇개의 데이터를 가져올지
+  @IsNumber()
+  @IsOptional()
+  take: number = 20;
+}
