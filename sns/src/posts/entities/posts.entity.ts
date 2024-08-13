a385e5e2eb604d583948132aae3f59fd3c6c713d
Yuhyeon0516 <rladbgus0516@naver.com>
2024-08-13 12:52:44 +0900

    Created function. If request for get method to image path, response for static image path.

--- sns/src/posts/entities/posts.entity.ts
+++ sns/src/posts/entities/posts.entity.ts
@@ -1,4 +1,7 @@
+import { Transform } from 'class-transformer';
 import { IsString } from 'class-validator';
+import { join } from 'path';
+import { POST_PUBLIC_IMAGE_PATH } from 'src/common/const/path.const';
 import { BaseModel } from 'src/common/entity/base.entity';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { UsersModel } from 'src/users/entities/users.entity';
@@ -26,6 +29,8 @@ export class PostsModel extends BaseModel {
   @Column({
     nullable: true,
   })
+  // image 이름이 아닌 /public/posts/{image}로 경로를 저장
+  @Transform(({ value }) => value && `/${join(POST_PUBLIC_IMAGE_PATH, value)}`)
   image?: string;
 
   @Column()
