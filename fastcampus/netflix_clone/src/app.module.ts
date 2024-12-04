5051b54d4425d6a3cfdb47d9fcaf6334843b7571
Yuhyeon0516 <rladbgus0516@naver.com>
2024-12-05 00:43:49 +0900

    Guard: Auth Guard 생성

--- fastcampus/netflix_clone/src/app.module.ts
+++ fastcampus/netflix_clone/src/app.module.ts
@@ -19,6 +19,8 @@ import { UserModule } from './user/user.module';
 import { User } from './user/entity/user.entity';
 import { envVariablesKeys } from './common/const/env.const';
 import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/guard/auth.guard';
 
 @Module({
   imports: [
@@ -55,6 +57,12 @@ import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware
     GenreModule,
     AuthModule,
     UserModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
 })
 export class AppModule implements NestModule {
