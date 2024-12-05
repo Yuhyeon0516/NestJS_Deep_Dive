0663ce6fd946babc80447ca6c8f1f3ddd1feef08
Yuhyeon0516 <rladbgus0516@naver.com>
2024-12-05 20:19:33 +0900

    Exception Filter: Forbidden Exception Filter

--- fastcampus/netflix_clone/src/app.module.ts
+++ fastcampus/netflix_clone/src/app.module.ts
@@ -19,10 +19,11 @@ import { UserModule } from './user/user.module';
 import { User } from './user/entity/user.entity';
 import { envVariablesKeys } from './common/const/env.const';
 import { BearerTokenMiddleware } from './auth/middleware/bearer-token.middleware';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { RbacGuard } from './auth/guard/rbac.guard';
 import { ResponseTimeinterceptor } from './common/interceptor/response-time.interceptor';
+import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
 
 @Module({
   imports: [
@@ -72,6 +73,10 @@ import { ResponseTimeinterceptor } from './common/interceptor/response-time.inte
     {
       provide: APP_INTERCEPTOR,
       useClass: ResponseTimeinterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ForbiddenExceptionFilter,
     },
   ],
 })
