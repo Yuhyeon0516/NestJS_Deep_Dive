fa4d17fd131e73eac00053a0d8cf5daeea2cb296
Yuhyeon0516 <rladbgus0516@naver.com>
2024-12-06 17:07:35 +0900

    Task Scheduling: Cron으로 좋아요 싫어요 통계기능

--- fastcampus/netflix_clone/src/common/common.module.ts
+++ fastcampus/netflix_clone/src/common/common.module.ts
@@ -6,6 +6,8 @@ import { diskStorage } from 'multer';
 import { join } from 'path';
 import { v4 } from 'uuid';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from 'src/movie/entity/movie.entity';
 
 @Module({
   imports: [
@@ -24,6 +26,7 @@ import { TasksService } from './tasks.service';
         },
       }),
     }),
+    TypeOrmModule.forFeature([Movie]),
   ],
   controllers: [CommonController],
   providers: [CommonService, TasksService],
